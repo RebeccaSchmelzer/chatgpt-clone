8f93a1d80d5600e221d30c7360bd7c6069a53652
Rebecca <beca.dance@gmail.com>
2023-02-02 17:58:51 -0500

    image generator!

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,26 @@ import { Configuration, OpenAIApi } from 'openai'
 import './App.css'
 
 function App() {
-  console.log(import.meta.env.VITE_Open_AI_Key);
-  // const configuration = new Configuration({
-  //   apiKey: process.env.VITE_Open_AI_Key,
-  // });
+  const configuration = new Configuration({
+    apiKey:import.meta.env.VITE_Open_AI_Key ,
+  });
+
+  const openai = new OpenAIApi(configuration); //init openai api
+  const generateImage = async () => {
+    const res = await openai.createImage({
+      prompt: "Say this is a test",
+      n: 1,
+      size: '1024x1024',
+
+    });
+    console.log(res.data.data[0].url);
+  }
+  
+
 
   return (
     <div className="App">
-      
+      <button onClick={generateImage}>Generate Image</button>
     </div>
   )
 }
